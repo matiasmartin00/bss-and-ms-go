c348f8ddf024bde1dca5491cae609cbe4234372c
matiasmartin <matiass.lean@gmail.com>
2024-03-03 17:55:15 +0100

    feat: add 1.5.6 example

--- mian.go
+++ mian.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/matiasmartin00/bss-and-ms-go/pointers"
+import (
+	"github.com/matiasmartin00/bss-and-ms-go/pointers"
+	"github.com/matiasmartin00/bss-and-ms-go/slices"
+)
 
 func main() {
 	// execute pointers 1.5.3
 	pointers.Execute()
+
+	// execute slices 1.5.6
+	slices.Execute()
 }
---
+++ slices/slices.go
@@ -0,0 +1,24 @@
+package slices
+
+import "fmt"
+
+func Execute() {
+	example()
+}
+
+func example() {
+	// declare array int with size 5
+	s1 := make([]int, 5)
+
+	// declare array int with size 0, capacity 5
+	s2 := make([]int, 0, 5)
+
+	s1 = append(s1, 1)
+	s2 = append(s2, 2)
+
+	// how we added an element, the length is 6 and the capacity is 10 (double)
+	fmt.Printf("S1: Length %d, Capacity %d. \n", len(s1), cap(s1))
+
+	// how we added an element, in this case the length is 1 and the capacity is 5
+	fmt.Printf("S2: Length %d, Capacity %d. \n", len(s2), cap(s2))
+}
